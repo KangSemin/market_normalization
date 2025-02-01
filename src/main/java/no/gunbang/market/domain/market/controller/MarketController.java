2fca96ddf80c15bab4bf26ae8a75c2c2410b4b06
gkdl4239 <gkdl4239@gmail.com>
2025-02-01 17:25:53 +0900

    ✨ feat: MarketController에 아이템 등록, 구매, 취소 메서드 구현

--- src/main/java/no/gunbang/market/domain/market/controller/MarketController.java
+++ src/main/java/no/gunbang/market/domain/market/controller/MarketController.java
@@ -1,11 +1,19 @@
 package no.gunbang.market.domain.market.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import no.gunbang.market.domain.market.dto.MarketTradeRequestDto;
+import no.gunbang.market.domain.market.dto.MarketRegisterRequestDto;
 import no.gunbang.market.domain.market.dto.MarketResponseDto;
 import no.gunbang.market.domain.market.service.MarketService;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +33,40 @@ public class MarketController {
     ) {
         Page<MarketResponseDto> allMarkets = marketService.getAllMarkets(page, size, name);
         return ResponseEntity.ok(allMarkets);
+    }
+
+    @PostMapping("/items")
+    public ResponseEntity<MarketResponseDto> registerMarket(
+        @RequestBody MarketRegisterRequestDto registerRequestDto,
+        HttpServletRequest req
+    ) {
+        Long sessionUserId = getSessionId(req);
+        MarketResponseDto marketResponseDto = marketService.registerMarket(sessionUserId, registerRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(marketResponseDto);
+    }
+
+    @PostMapping("/items/trade")
+    public ResponseEntity<MarketResponseDto> tradeMarket(
+        @RequestParam MarketTradeRequestDto tradeRequestDto,
+        HttpServletRequest req
+    ) {
+        Long sessionUserId = getSessionId(req);
+        MarketResponseDto marketResponseDto = marketService.tradeMarket(sessionUserId, tradeRequestDto);
+        return ResponseEntity.ok(marketResponseDto);
+    }
+
+    @DeleteMapping("/items/{marketId}")
+    public ResponseEntity<String> deleteMarket(
+        @PathVariable Long marketId,
+        HttpServletRequest req
+    ) {
+        Long sessionUserId = getSessionId(req);
+        marketService.deleteMarket(sessionUserId, marketId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("삭제완료");
+    }
+
+
+    private Long getSessionId(HttpServletRequest req) {
+        return (Long) req.getSession().getAttribute("userId");
     }
 }
