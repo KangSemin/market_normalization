b4a14664d6aa2db26a486f5b060feb9c9ed11c70
gkdl4239 <gkdl4239@gmail.com>
2025-02-01 22:57:41 +0900

    ♻️ refactor: 쿼리에서 바로 ResponseDto 반환하게끔 변경

--- src/main/java/no/gunbang/market/domain/market/repository/MarketRepositoryCustom.java
+++ src/main/java/no/gunbang/market/domain/market/repository/MarketRepositoryCustom.java
@@ -3,6 +3,7 @@ package no.gunbang.market.domain.market.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 import no.gunbang.market.domain.market.dto.MarketListResponseDto;
+import no.gunbang.market.domain.market.dto.MarketResponseDto;
 import no.gunbang.market.domain.market.entity.Market;
 import no.gunbang.market.domain.market.entity.Trade;
 import org.springframework.data.domain.Page;
@@ -10,7 +11,7 @@ import org.springframework.data.domain.Pageable;
 
 public interface MarketRepositoryCustom {
 
-    Page<Market> findAllMarkets(String name, Pageable pageable);
+    Page<MarketResponseDto> findAllMarkets(String name, Pageable pageable);
     List<Trade> findUserMarketHistory(Long userId);
 
     Page<MarketListResponseDto> findPopularTradeItems(LocalDateTime startDate, Pageable pageable);
