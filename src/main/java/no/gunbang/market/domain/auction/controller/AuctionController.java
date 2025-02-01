c26311b56e5c7772c8279052c485e084abe44568
backswan0 <rnrwprh3434@gmail.com>
2025-02-01 20:17:23 +0900

    :sparkles: feat/jh-main-domain-auction
    - 경매장에 아이템을 등록하는 C 구현 완료
    
    - 생성자 대신 정적 팩토리 메서드 사용
    
    - 경매 희망 기간이 3일 미만 또는 7일 초과일 때 예외 처리 반영
    
    - int 타입인 경매 희망 기간을 LocalDateTime으로 변환하는 private 메서드를 Auction 엔티티에 생성 및 활용

--- src/main/java/no/gunbang/market/domain/auction/controller/AuctionController.java
+++ src/main/java/no/gunbang/market/domain/auction/controller/AuctionController.java
@@ -1,10 +1,30 @@
 package no.gunbang.market.domain.auction.controller;
 
+import lombok.RequiredArgsConstructor;
+import no.gunbang.market.domain.auction.dto.request.CreateAuctionRequestDto;
+import no.gunbang.market.domain.auction.dto.response.CreateAuctionResponseDto;
+import no.gunbang.market.domain.auction.service.AuctionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auctions")
+@RequiredArgsConstructor
 public class AuctionController {
+
+    private final AuctionService auctionService;
+
+    @PostMapping
+    public ResponseEntity<CreateAuctionResponseDto> createAuction(
+        @RequestBody CreateAuctionRequestDto requestDto
+    ) {
+        CreateAuctionResponseDto responseDto = auctionService.saveAuction(requestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
 
 }
--- src/main/java/no/gunbang/market/domain/auction/service/AuctionService.java
+++ src/main/java/no/gunbang/market/domain/auction/service/AuctionService.java
@@ -1,8 +1,59 @@
 package no.gunbang.market.domain.auction.service;
 
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import no.gunbang.market.common.Item;
+import no.gunbang.market.common.ItemRepository;
+import no.gunbang.market.common.exception.CustomException;
+import no.gunbang.market.common.exception.ErrorCode;
+import no.gunbang.market.domain.auction.dto.request.CreateAuctionRequestDto;
+import no.gunbang.market.domain.auction.dto.response.CreateAuctionResponseDto;
+import no.gunbang.market.domain.auction.entity.Auction;
+import no.gunbang.market.domain.auction.repository.AuctionRepository;
+import no.gunbang.market.domain.user.entity.User;
+import no.gunbang.market.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuctionService {
 
+    private final AuctionRepository auctionRepository;
+    private final ItemRepository itemRepository;
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    public CreateAuctionResponseDto saveAuction(
+        CreateAuctionRequestDto requestDto
+    ) {
+        Long userId = (Long) httpSession.getAttribute("userId");
+
+        User foundUser = userRepository.findById(userId)
+            .orElseThrow(
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
+            );
+
+        Item foundItem = itemRepository.findById(requestDto.getItemId())
+            .orElseThrow(
+                () -> new CustomException(ErrorCode.ITEM_NOT_FOUND)
+            );
+
+        boolean isAuctionDaysOutOfRange = requestDto.getAuctionDays() < 3
+            || 7 < requestDto.getAuctionDays();
+
+        if (isAuctionDaysOutOfRange) {
+            throw new CustomException(ErrorCode.AUCTION_DAYS_OUT_OF_RANGE);
+        }
+
+        Auction auctionToSave = Auction.of(
+            foundUser,
+            foundItem,
+            requestDto.getStartingPrice(),
+            requestDto.getAuctionDays()
+        );
+
+        Auction savedAuction = auctionRepository.save(auctionToSave);
+
+        return CreateAuctionResponseDto.toDto(savedAuction);
+    }
 }
