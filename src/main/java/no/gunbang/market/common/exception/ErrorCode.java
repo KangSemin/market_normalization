3d4b903e3294fc6d7f2bdd8fb85c9ee305d5f7af
gkdl4239 <gkdl4239@gmail.com>
2025-02-01 20:45:22 +0900

    ✨ feat: 에러코드 추가

--- src/main/java/no/gunbang/market/common/exception/ErrorCode.java
+++ src/main/java/no/gunbang/market/common/exception/ErrorCode.java
@@ -12,7 +12,19 @@ public enum ErrorCode {
 
     PAGING_ERROR(HttpStatus.BAD_REQUEST, "페이지 입력값이 잘못되었습니다."),
 
-    WRONG_EMAIL_OR_PASSWORD(HttpStatus.UNAUTHORIZED, "이메일이나 비밀번호를 잘못 입력하였습니다.");
+    WRONG_EMAIL_OR_PASSWORD(HttpStatus.UNAUTHORIZED, "이메일이나 비밀번호를 잘못 입력하였습니다."),
+
+    ITEM_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 아이템입니다."),
+
+    MARKET_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 거래입니다."),
+
+    INVENTORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 인벤토리입니다."),
+
+    LACK_OF_GOLD(HttpStatus.BAD_REQUEST, "골드가 부족합니다."),
+
+    LACK_OF_SELLER_INVENTORY(HttpStatus.BAD_REQUEST, "판매자의 재고가 부족합니다.");
+
+
 
     private final HttpStatus status;
     private final String message;
