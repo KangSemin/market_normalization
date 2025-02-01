91f708b3bb02d5d3c114c01662943f3bb68cd915
gkdl4239 <gkdl4239@gmail.com>
2025-02-01 22:57:13 +0900

    ✨ feat: itemId로 검색했을때 같은 아이템을 가지고있는 거래 리스트 호출하는 메서드 구현

--- src/main/java/no/gunbang/market/domain/market/repository/MarketRepository.java
+++ src/main/java/no/gunbang/market/domain/market/repository/MarketRepository.java
@@ -1,7 +1,10 @@
 package no.gunbang.market.domain.market.repository;
 
+import java.util.List;
 import no.gunbang.market.domain.market.entity.Market;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MarketRepository extends JpaRepository<Market, Long>, MarketRepositoryCustom {
+
+    List<Market> findByItemIdOrderByPriceAsc(Long itemId);
 }
