c26607ffefef44070e138a1550502afe94541c34
backswan0 <rnrwprh3434@gmail.com>
2025-02-02 02:28:49 +0900

    :sparkles: feat/jh-main-domain-auction (Bid entity)
    - 마지막 입찰 성공 시간 속성 추가
    
    - 기존에 등록된 입찰의 입찰 가격과 사용자를 수정하는 메서드를 엔티티 안에 추가
    
    - 레포지토리에 경매에 등록된 입찰을 조회하는 메서드 추가

--- src/main/java/no/gunbang/market/domain/auction/entity/Bid.java
+++ src/main/java/no/gunbang/market/domain/auction/entity/Bid.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import no.gunbang.market.common.BaseEntity;
@@ -30,6 +31,9 @@ public class Bid extends BaseEntity {
     @Comment("입찰 가격")
     private long bidPrice;
 
+    @Comment("마지막 입찰 성공 시간")
+    private LocalDateTime updatedAt;
+
     @Comment("경매 외래키")
     @ManyToOne
     @JoinColumn(name = "auction_id")
@@ -39,4 +43,25 @@ public class Bid extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public static Bid of(
+        User user,
+        Auction auction,
+        long bidPrice
+    ) {
+        Bid bid = new Bid();
+        bid.user = user;
+        bid.auction = auction;
+        bid.bidPrice = bidPrice;
+        return bid;
+    }
+
+    public void updateBid(
+        long bidPrice,
+        User user
+    ) {
+        this.bidPrice = bidPrice;
+        this.user = user;
+        this.updatedAt = LocalDateTime.now();
+    }
 }
