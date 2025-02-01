09446ba0496121ce452491b828cd1353cb156793
gkdl4239 <gkdl4239@gmail.com>
2025-02-01 22:59:06 +0900

    ✨ feat: 아이템 기준으로 그룹화시켜서 최저가인 거래만 꺼내 Dto로 변환후 반환하는 메서드로 변경

--- src/main/java/no/gunbang/market/domain/market/repository/MarketRepositoryImpl.java
+++ src/main/java/no/gunbang/market/domain/market/repository/MarketRepositoryImpl.java
@@ -5,19 +5,20 @@ import static no.gunbang.market.domain.market.entity.QMarket.market;
 import static no.gunbang.market.domain.market.entity.QTrade.trade;
 import static no.gunbang.market.domain.user.entity.QUser.user;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import no.gunbang.market.common.Status;
 import no.gunbang.market.domain.market.dto.MarketListResponseDto;
-import no.gunbang.market.domain.market.entity.Market;
+import no.gunbang.market.domain.market.dto.MarketResponseDto;
 import no.gunbang.market.domain.market.entity.QMarket;
 import no.gunbang.market.domain.market.entity.QTrade;
 import no.gunbang.market.domain.market.entity.Trade;
+import no.gunbang.market.domain.user.dto.UserResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -66,38 +67,52 @@ public class MarketRepositoryImpl implements MarketRepositoryCustom {
     }
 
     @Override
-    public Page<Market> findAllMarkets(String name, Pageable pageable) {
+    public Page<MarketResponseDto> findAllMarkets(String name, Pageable pageable) {
 
-        JPAQuery<Market> query = queryFactory.selectFrom(market)
-            .join(market.user, user).fetchJoin()
-            .join(market.item, item).fetchJoin()
-            .where(market.status.eq(Status.ON_SALE));
-
+        BooleanBuilder conditions = new BooleanBuilder();
+        conditions.and(market.status.eq(Status.ON_SALE));
         if (name != null && !name.trim().isEmpty()) {
-            query.where(market.item.name.contains(name));
+            conditions.and(market.item.name.contains(name));
         }
 
-        List<Market> markets = query
+        // 각 아이템 별로 최저가 MarketResponseDto 생성
+        List<MarketResponseDto> markets = queryFactory
+            .select(Projections.constructor(
+                MarketResponseDto.class,
+                market.id,
+                market.amount,
+                market.price.min(),
+                market.status,
+                Projections.constructor(UserResponseDto.class,
+                    user.id, user.nickname, user.gold),
+                market.item
+            ))
+            .from(market)
+            .join(market.user, user)
+            .join(market.item, item)
+            .where(conditions)
+            .groupBy(market.item.name)
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
             .fetch();
 
-        return new PageImpl<>(
-            markets,
-            pageable,
-            getMarketsTotalCount(name)
-            );
+        return new PageImpl<>(markets, pageable, getMarketsTotalCount(name));
     }
 
+    // 페이징에 필요한 총 개수
     private Long getMarketsTotalCount(String name) {
 
-        JPAQuery<Long> countQuery = queryFactory.select(market.count())
-            .from(market);
+        BooleanBuilder conditions = new BooleanBuilder();
+        conditions.and(market.status.eq(Status.ON_SALE));
 
         if (name != null && !name.trim().isEmpty()) {
-            countQuery.where(market.item.name.contains(name));
+            conditions.and(market.item.name.contains(name));
         }
 
-        return countQuery.fetchOne();
+        return queryFactory
+            .select(market.item.name.countDistinct())
+            .from(market)
+            .where(conditions)
+            .fetchOne();
     }
 }
