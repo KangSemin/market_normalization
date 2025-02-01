bb85efc2f873a007b8397f3ec419c77b77c6bc75
gkdl4239 <gkdl4239@gmail.com>
2025-02-01 17:45:39 +0900

    🔥 delete: Inventory/Item Repository 패키지 이동에 따른 삭제

--- src/main/java/no/gunbang/market/domain/market/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package no.gunbang.market.domain.market.repository;
-
-import no.gunbang.market.common.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
-}
