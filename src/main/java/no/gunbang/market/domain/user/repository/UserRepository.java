40724d2236d4bf85aa4c0c6c06b98531630c3133
roqkfchqh <plmokn48@daum.net>
2025-02-01 15:19:15 +0900

    🐛 fix: redis 주석처리, user login 시 사용되는 findIdByEmail메서드 수정

--- src/main/java/no/gunbang/market/domain/user/repository/UserRepository.java
+++ src/main/java/no/gunbang/market/domain/user/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("SELECT u.id FROM User u WHERE u.email = :email")
     Long findIdByEmail(String email);
 
     @Query("SELECT u.password FROM User u WHERE u.id = :id")
