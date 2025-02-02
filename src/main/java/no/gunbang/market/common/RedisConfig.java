8f0a36054bac84a4b6f7386e4f1b70c7c3eddece
roqkfchqh <plmokn48@daum.net>
2025-02-02 16:34:14 +0900

    📝chore: redis 주석 해제

--- src/main/java/no/gunbang/market/common/RedisConfig.java
+++ src/main/java/no/gunbang/market/common/RedisConfig.java
@@ -1,13 +1,9 @@
-//package no.gunbang.market.common;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-//
-//@Configuration
-//@EnableRedisHttpSession //세션유지 30분
-//public class RedisConfig {
-//}
+package no.gunbang.market.common;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-//TODO: 유저권한인증 AOP개발, 현재 userId가 null일시 500에러 뱉음
-//TODO: redis 관련 주석은 docker 사용 가능 할 때부터 해제할 것.
+@Configuration
+@EnableRedisHttpSession //세션유지 30분
+public class RedisConfig {
+}
