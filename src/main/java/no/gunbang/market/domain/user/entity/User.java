e8b6fdf4669ecc34f78cae6cb6b2f1bdfb9d369e
gkdl4239 <gkdl4239@gmail.com>
2025-02-02 22:03:15 +0900

    ♻️ refactor: 구매자의 잔액을 확인하고 구매금액만큼 차감하는 로직 추가

--- src/main/java/no/gunbang/market/domain/user/entity/User.java
+++ src/main/java/no/gunbang/market/domain/user/entity/User.java
@@ -44,9 +44,10 @@ public class User {
     private String email;
     private String password;
 
-    public void validateGold(long totalPrice) {
+    public void decreaseGold(long totalPrice) {
         if (getGold() < totalPrice) {
             throw new CustomException(ErrorCode.LACK_OF_GOLD);
         }
+        gold -= totalPrice;
     }
 }
