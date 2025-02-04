68ec9f9b675ca055ae0c93deb3c8561d58295a9d
backswan0 <rnrwprh3434@gmail.com>
2025-02-05 03:29:48 +0900

    :bug::test_tube: fix&test/jh-test-auction service
    - 락 어노테이션을 사용하여 비관적 락 적용
    
    - 동시성 문제 해결 여부를 확인하는 테스트 코드 리팩토링

--- src/main/java/no/gunbang/market/domain/auction/repository/BidRepository.java
+++ src/main/java/no/gunbang/market/domain/auction/repository/BidRepository.java
@@ -1,15 +1,19 @@
 package no.gunbang.market.domain.auction.repository;
 
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import no.gunbang.market.domain.auction.entity.Auction;
 import no.gunbang.market.domain.auction.entity.Bid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BidRepository extends JpaRepository<Bid, Long> {
-
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Bid> findByAuction(Auction auction);
 
     boolean existsByAuctionId(Long auctionId);
 
+    @Transactional
     void deleteAllByAuction(Auction auction);
 }
