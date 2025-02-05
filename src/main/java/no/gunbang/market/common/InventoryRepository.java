de636af1da63cde18089f774b7ebfdb00071d9f7
gkdl4239 <gkdl4239@gmail.com>
2025-02-05 22:23:36 +0900

    ✨ feat: 낙관적 락 사용을 위한 조회시 어노테이션 추가

--- src/main/java/no/gunbang/market/common/InventoryRepository.java
+++ src/main/java/no/gunbang/market/common/InventoryRepository.java
@@ -1,9 +1,17 @@
 package no.gunbang.market.common;
 
+import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
     Optional<Inventory> findByUserIdAndItemId(Long userId, Long itemId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select i from Inventory i where i.user.id = :userId and i.item.id = :itemId")
+    Optional<Inventory> findByUserIdAndItemIdForUpdate(@Param("userId") Long userId, @Param("itemId") Long itemId);
 }
