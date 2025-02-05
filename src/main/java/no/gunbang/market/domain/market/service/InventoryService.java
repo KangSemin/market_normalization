6079b794cdc8bea30a4a722e764981e3f2af9e20
gkdl4239 <gkdl4239@gmail.com>
2025-02-05 22:25:01 +0900

    ♻️ refactor: 커스터마이징한 락 사용 삭제

--- src/main/java/no/gunbang/market/domain/market/service/InventoryService.java
+++ src/main/java/no/gunbang/market/domain/market/service/InventoryService.java
@@ -6,25 +6,24 @@ import no.gunbang.market.common.InventoryRepository;
 import no.gunbang.market.common.Item;
 import no.gunbang.market.common.exception.CustomException;
 import no.gunbang.market.common.exception.ErrorCode;
-import no.gunbang.market.common.lock.LockStrategy;
 import no.gunbang.market.domain.user.entity.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
-    private final LockStrategy lockStrategy;
 
+    @Transactional
     public void updateInventory(Item item, User user, int amount) {
 
-        Inventory inventory = inventoryRepository.findByUserIdAndItemId(user.getId(), item.getId())
+        Inventory inventory = inventoryRepository
+            .findByUserIdAndItemIdForUpdate(user.getId(), item.getId())
             .orElse(null);
 
-        // 아이템 갖고있지 않았다면 판매자는 예외 구매자는 새로 객체 생성
         if (inventory == null) {
-
             if (amount < 0) {
                 throw new CustomException(ErrorCode.LACK_OF_SELLER_INVENTORY);
             }
@@ -33,19 +32,10 @@ public class InventoryService {
             return;
         }
 
-        final Inventory currentInventory = inventory;
-        // 구매자는 아이템 수 증가 판매자는 감소 후 setter 호출
-        lockStrategy.execute(
-            Inventory.class,
-            inventory.getClass().getSimpleName() + ":" + inventory.getId(), () -> {
-            int newAmount = currentInventory.getAmount() + amount;
-            if (newAmount < 0) {
-                throw new CustomException(ErrorCode.LACK_OF_SELLER_INVENTORY);
-            }
-            currentInventory.setAmount(newAmount);
-            inventoryRepository.save(currentInventory);
-            return null;
-        });
+        int newAmount = inventory.getAmount() + amount;
+        if (newAmount < 0) {
+            throw new CustomException(ErrorCode.LACK_OF_SELLER_INVENTORY);
+        }
+        inventory.setAmount(newAmount);
     }
-
 }
