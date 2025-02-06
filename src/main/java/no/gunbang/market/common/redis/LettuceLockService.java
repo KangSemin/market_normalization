ad997902df0eda3757cbb933fe0dd6c874a6c094
gkdl4239 <gkdl4239@gmail.com>
2025-02-06 17:38:44 +0900

    🎉 add: Lettuce 락 구현

---
+++ src/main/java/no/gunbang/market/common/redis/LettuceLockService.java
@@ -0,0 +1,23 @@
+package no.gunbang.market.common.redis;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class LettuceLockService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public Boolean lock(Object key) {
+        return redisTemplate
+            .opsForValue()
+            .setIfAbsent(key.toString(), "lock", Duration.ofMillis(3000)); // 현재는 3000ms로 고정
+    }
+
+    public Boolean unlock(Object key) {
+        return redisTemplate.delete(key.toString());
+    }
+}
