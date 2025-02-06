976f95fae0c0722311718ef878e25ca1e8604a19
roqkfchqh <plmokn48@daum.net>
2025-02-06 18:21:43 +0900

    ♻️ refactor: 로직 일부 수정

--- src/main/java/no/gunbang/market/domain/market/cursor/AmountCursorStrategy.java
+++ src/main/java/no/gunbang/market/domain/market/cursor/AmountCursorStrategy.java
@@ -4,7 +4,6 @@ import com.querydsl.core.types.Order;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.Expressions;
 import no.gunbang.market.common.CursorStrategy;
-import no.gunbang.market.common.QItem;
 import no.gunbang.market.domain.market.entity.QMarket;
 
 public class AmountCursorStrategy implements CursorStrategy<MarketCursorValues> {
@@ -16,12 +15,12 @@ public class AmountCursorStrategy implements CursorStrategy<MarketCursorValues> 
         if (Order.DESC.equals(order)) {
             return Expressions.booleanTemplate(
                     "(SUM({0}) < {1}) OR (SUM({0}) = {1} AND {2} < {3})",
-                    QMarket.market.amount, lastAmount, QItem.item.id, lastItemId
+                    QMarket.market.amount, lastAmount, QMarket.market.item.id, lastItemId
             );
         } else {
             return Expressions.booleanTemplate(
                     "(SUM({0}) > {1}) OR (SUM({0}) = {1} AND {2} > {3})",
-                    QMarket.market.amount, lastAmount, QItem.item.id, lastItemId
+                    QMarket.market.amount, lastAmount, QMarket.market.item.id, lastItemId
             );
         }
     }
