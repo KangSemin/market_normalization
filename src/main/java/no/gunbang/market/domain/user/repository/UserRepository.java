b852d54c9829bb64f9661b92d93a4b602a6b2ec9
roqkfchqh <plmokn48@daum.net>
2025-02-07 11:44:23 +0900

    🐛 fix: user가 null일 경우 500에러 날 가능성 차단

--- src/main/java/no/gunbang/market/domain/user/repository/UserRepository.java
+++ src/main/java/no/gunbang/market/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package no.gunbang.market.domain.user.repository;
 
 import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import no.gunbang.market.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -10,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email")
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select u from User u where u.id = :id")
--- src/main/java/no/gunbang/market/domain/user/service/UserService.java
+++ src/main/java/no/gunbang/market/domain/user/service/UserService.java
@@ -18,7 +18,9 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     public Long login(LoginRequestDto dto){
-        User user = userRepository.findByEmail(dto.getEmail());
+        User user = userRepository.findByEmail(dto.getEmail())
+            .orElseThrow(() -> new CustomException(ErrorCode.WRONG_EMAIL_OR_PASSWORD));
+
         if(user.getId() == null || !passwordEncoder.matches(dto.getPassword(), user.getPassword())){
             throw new CustomException(ErrorCode.WRONG_EMAIL_OR_PASSWORD);
         }
